9effecc24fcbab7e3fbd3eb8443313ab9e89c2c9
Elena Nardi <elenanardi@hotmail.com>
2023-04-03 17:34:49 +0200

    Post request pour récuperation ID client

--- app/.ipynb_checkpoints/app-checkpoint.py
+++ app/.ipynb_checkpoints/app-checkpoint.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import json
-import requests
+#import requests
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,8 +8,15 @@ app.app_context().push()
 
 @app.route("/get_clients_id")
 def get_clients_id_list():
-    data = pd.read_csv('../train_ab.csv', index_col=0)
+    data = pd.read_csv('../../train_ab.csv', index_col=0)
     return jsonify({'data': list(data.index)[:200]})
+
+@app.route("/predict", methods= ["POST", "GET"])
+def predict():
+    if request.method == 'POST':
+        data = request.get_data()
+        print(data)
+        return 'hello'
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
