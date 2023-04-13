73fe36a5a6ab027ffd5c396ab1e1734bdeb07a19
Elena Nardi <elenanardi@hotmail.com>
2023-04-13 14:47:35 +0200

    ajout tests unitaires

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,18 +1,18 @@
 import unittest
 from app import predict
 import json
-
-class TestApp(unittest.TestCase):
-    def test_should_return_client_list(self):
-        client_list = get_clients_id_list().json
-        expected_length = 200
-        self.assertEqual(len(client_list['data']), 200)
+import requests
+url = 'http://127.0.0.1:3000/predict'
         
 class TestPrediction(unittest.TestCase):
-    def test_should_predict_1(self):
-        client_id = str(12345)
+    def test_should_predict_60(self):
+        client_id = str(100001)
         prediction = requests.post(url, data = client_id)
-        self.assertEqual(len(client_list['data']), 200)
+        self.assertEqual(prediction.text, str(60))
+    def test_should_predict_31(self):
+        client_id = str(100005)
+        prediction = requests.post(url, data = client_id)
+        self.assertEqual(prediction.text, str(31))
         
         
 if __name__ == "__main__":
