3284ffdc7c6b36578b5a1b316a80a9ea777aa7df
Elena Nardi <elenanardi@hotmail.com>
2023-04-18 06:29:38 +0200

    mise à jour tests

--- Tests/.ipynb_checkpoints/tests-checkpoint.py
+++ Tests/.ipynb_checkpoints/tests-checkpoint.py
@@ -1,19 +1,23 @@
-import unittest
 from app import predict
 import json
 import requests
+import unittest
+
 url = 'http://127.0.0.1:3000/predict'
-        
+
+# testing the value of the prediction
 class TestPrediction(unittest.TestCase):
+
     def test_should_predict_60(self):
         client_id = str(100001)
-        prediction = requests.post(url, data = client_id)
+        prediction = requests.post(url, data=client_id)
         self.assertEqual(prediction.text, str(60))
+
     def test_should_predict_31(self):
         client_id = str(100005)
-        prediction = requests.post(url, data = client_id)
+        prediction = requests.post(url, data=client_id)
         self.assertEqual(prediction.text, str(31))
-        
-        
+
+
 if __name__ == "__main__":
     unittest.main()
