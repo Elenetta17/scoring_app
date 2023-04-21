91ab49e1edfdd8e9c7d9161a01146358e93b1da8
Elena Nardi <elenanardi@hotmail.com>
2023-04-21 10:47:49 +0200

    mise à jour modèle et tests

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -13,12 +13,12 @@ app.app_context().push()
 
 
 # prediction function
-@app.route('/predict', methods=["POST", "GET"])
+@app.route("/predict", methods=["POST", "GET"])
 def predict():
-    if request.method == 'POST':
+    if request.method == "POST":
         client_id = request.get_data()  # get client_id
-        data = pd.read_csv('test_kaggle_reduced.csv', index_col=0)  # read data
-        loaded_model = pickle.load(open('model.pkl', 'rb'))  # load model
+        data = pd.read_csv("test_kaggle_reduced.csv", index_col=0)  # read data
+        loaded_model = pickle.load(open("model.pkl", "rb"))  # load model
         result = loaded_model.predict_proba(
             data.loc[int(client_id)].values.reshape(1, -1))  # prediction
         proba_remboursement = np.int64(result[0][0]*100)  # prediction as %
--- Tests/.ipynb_checkpoints/tests-checkpoint.py
+++ Tests/.ipynb_checkpoints/tests-checkpoint.py
@@ -3,20 +3,20 @@ import json
 import requests
 import unittest
 
-url = 'http://127.0.0.1:3000/predict'
+url = "http://127.0.0.1:3000/predict"
 
 # testing the value of the prediction
 class TestPrediction(unittest.TestCase):
 
-    def test_should_predict_60(self):
+    def test_should_predict_79(self):
         client_id = str(100001)
         prediction = requests.post(url, data=client_id)
-        self.assertEqual(prediction.text, str(60))
+        self.assertEqual(prediction.text, str(79))
 
-    def test_should_predict_31(self):
+    def test_should_predict_50(self):
         client_id = str(100005)
         prediction = requests.post(url, data=client_id)
-        self.assertEqual(prediction.text, str(31))
+        self.assertEqual(prediction.text, str(50))
 
 
 if __name__ == "__main__":
